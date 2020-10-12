5cbf259ab2631678349d97598b030d1e7faf691a
gustavo <imartytk@Hotmail.com>
2020-10-12 13:55:15 -0300

    changes

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import './src/config/reactotronConfig';
 import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider, useSelector } from 'react-redux';
-import { StatusBar } from 'react-native';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { darken } from 'polished';
-import { appStore, appPersistor } from './src/appStore/appStore';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider, useSelector} from 'react-redux';
+import {StatusBar} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {darken} from 'polished';
+import {appStore, appPersistor} from './src/appStore/appStore';
 import createRouter from './routes';
-import { appColors } from './src/utils/appColors';
+import {appColors} from './src/utils/appColors';
+import {NavigationContainer} from '@react-navigation/native';
 
 function DevDoido() {
-  const signed = useSelector((state) => state.auth.signed);
+  const signed = useSelector(state => state.auth.signed);
   const Routes = createRouter(signed);
   return <Routes />;
 }
@@ -24,7 +25,9 @@ const App = () => {
           backgroundColor={darken(0.2, appColors.primary)}
         />
         <PaperProvider>
-          <DevDoido />
+          <NavigationContainer>
+            <DevDoido />
+          </NavigationContainer>
         </PaperProvider>
       </PersistGate>
     </Provider>
