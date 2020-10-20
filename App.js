2fb973bb261c4d5d307af057e65d02b3b5f4f52f
gustavo <imartytk@Hotmail.com>
2020-10-20 11:25:48 -0300

    tst

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import './src/config/reactotronConfig';
 import React from 'react';
-import {PersistGate} from 'redux-persist/integration/react';
-import {Provider, useSelector} from 'react-redux';
-import {StatusBar} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {darken} from 'polished';
-import {appStore, appPersistor} from './src/appStore/appStore';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider, useSelector } from 'react-redux';
+import { StatusBar } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { darken } from 'polished';
+import { appStore, appPersistor } from './src/appStore/appStore';
 import createRouter from './routes';
-import {appColors} from './src/utils/appColors';
-import {NavigationContainer} from '@react-navigation/native';
+import { appColors } from './src/utils/appColors';
 
 function DevDoido() {
-  const signed = useSelector(state => state.auth.signed);
+  const signed = useSelector((state) => state.auth.signed);
   const Routes = createRouter(signed);
   return <Routes />;
 }
@@ -20,16 +19,13 @@ const App = () => {
   return (
     <Provider store={appStore}>
       <PersistGate persistor={appPersistor}>
-        <DevDoido />
-        {/* <StatusBar
+        <StatusBar
           barStyle="light-content"
           backgroundColor={darken(0.2, appColors.primary)}
         />
         <PaperProvider>
-          <NavigationContainer>
-            <DevDoido />
-          </NavigationContainer>
-        </PaperProvider> */}
+          <DevDoido />
+        </PaperProvider>
       </PersistGate>
     </Provider>
   );
--- src/appNavigation/NavigatorSelector.js
+++ src/appNavigation/NavigatorSelector.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import AdminNavigator from './navigators/AdminNavigator';
-import {ClientNavigator} from './navigators/ClientNavigator';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {AppearanceProvider} from 'react-native-appearance';
+import ClientNavigator from './navigators/ClientNavigator';
+
 const NavigatorSelector = () => {
   const profile = useSelector(state => state.user.profile);
   if (profile.type === 'admin') {
     return <AdminNavigator />;
   }
-  return (
-    <SafeAreaProvider>
-      <AppearanceProvider>
-        <ClientNavigator />
-      </AppearanceProvider>
-    </SafeAreaProvider>
-  );
+  return <ClientNavigator></ClientNavigator>;
 };
 
 export default NavigatorSelector;
